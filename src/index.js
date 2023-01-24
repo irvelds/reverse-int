1248608dd89008d9c9ce23851c3ca757c8aa2493
irvelds <irvelds@mail.ru>
2023-01-24 13:12:56 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+let str = String(n).split('').reduce((acc, e) => acc = e+acc, '').replace(/[^0-9]/g, '');
+return Number(str)
 }
